b16e08b0a1c3726ea823d3bfd10ca0445f5fa374
YadavYashvant <yashvantyadav855@gmail.com>
2023-08-20 22:48:02 +0530

    added navigation.xml

--- app/src/main/java/com/example/smooth_bottom_bar/MainActivity.kt
+++ app/src/main/java/com/example/smooth_bottom_bar/MainActivity.kt
@@ -2,12 +2,28 @@ package com.example.smooth_bottom_bar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import me.ibrahimsn.lib.SmoothBottomBar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+        val homeFragment = HomeFragment()
+        val aboutFragment = AboutFragment()
+        val settingsFragment = SettingsFragment()
 
+        setCurrentFragment(homeFragment)
 
+    }
+
+    private fun setCurrentFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment,fragment)
+            commit()
+        }
     }
 }
